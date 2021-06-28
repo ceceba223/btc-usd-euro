472a7c1b0401ce956372e195b8208b06843937f2
ceceba223 <hagen-gerlatzek@web.de>
2021-06-28 13:44:18 +0200

    fully functional

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from usd_in_eur import usd_in_eur
 from btc_in_usd import btc_in_usd
 from btc_in_eur import btc_in_eur
 
+from save import save, setup
+import os.path
+
+import time
+
 def main():
-    BTC_USD = round(btc_in_usd(), 2)
-    USD_EUR = round(usd_in_eur(), 2)
-    print(BTC_USD)
-    print(USD_EUR)
-    print(round(btc_in_eur(BTC_USD, USD_EUR), 3))
-    
-    
-    #while True:
-    #    # Sleep for 24 hours 
-    #    # (For testing purposes you can set it to a lower number)
-    #    time.sleep(24 * 60 * 60)
+    if os.path.isfile('data.xlsx'):
+        pass
+    else:
+        setup()
+        
+    while True:
+        BTC_USD = round(btc_in_usd(), 2)
+        USD_EUR = round(usd_in_eur(), 2)
+        BTC_EUR = round(btc_in_eur(BTC_USD, USD_EUR), 2)
+
+        save(BTC_USD, BTC_EUR, USD_EUR)
+
+        # Sleep for 24 hours 
+        # (For testing purposes you can set it to a lower number)
+        time.sleep(24 * 60 * 60)
 
 if __name__ == '__main__':
     main()
--- save.py
+++ save.py
@@ -1,2 +1,24 @@
+from openpyxl import Workbook, load_workbook
+from datetime import date, datetime
+
 def save(BTC_USD, BTC_EUR, USD_EUR):
-    pass
+    workbook = load_workbook('data.xlsx')
+    sheet = workbook.active
+
+    date = datetime.now()
+
+    row = (date, BTC_USD, BTC_EUR, USD_EUR)
+    sheet.append(row)
+
+    workbook.save(filename="data.xlsx")
+
+def setup():
+    workbook = Workbook()
+    sheet = workbook.active
+
+    sheet["A1"] = "date"
+    sheet["B1"] = "1 BTC in USD"
+    sheet["C1"] = "1 BTC in USD"
+    sheet["D1"] = "1 USD in EUR"
+
+    workbook.save(filename="data.xlsx")
